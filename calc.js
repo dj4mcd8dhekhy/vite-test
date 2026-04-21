e5f27a06973477f164072fd06b4d153b94a11e33
BugTracker <arbete10001@outlook.com>
2026-04-21 14:33:27 +0200

    add calc functions file

---
+++ calc.js
@@ -0,0 +1,15 @@
+export function AddNums(a, b) {
+  return a + b;
+}
+
+export function SubNums(a, b) {
+  return a - b;
+}
+
+export function MulNums(a, b) {
+  return a * b;
+}
+
+export function DivNums(a, b) {
+  return a / b;
+}
